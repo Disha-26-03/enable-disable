ee33bd38d4415945c3789bccaefb1012cdcacdca
dishas@12 <saraswatdisha26@gmail.com>
2023-08-18 12:28:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    
+    const [initialData, setInitialData] = useState([
+        { attribute1: 'Value 1', attribute2: 'Value 2', attribute3: 'Value 3' },
+        { attribute1: 'Value 1', attribute2: 'Value 2', attribute3: 'Value 3' },
+        { attribute1: 'Value 1', attribute2: 'Value 2', attribute3: 'Value 3' },
+        { attribute1: 'Value 1', attribute2: 'Value 2', attribute3: 'Value 3' },
+        
+    ]);
+    
+    const [enable,setenable] = useState(false);    
+    function checkEnable()
+    {
+        setenable(true)
+        
+    }
+    function checkDisable()
+    {
+        setenable(false)
+    }
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Attribute 1</th>
+                        <th>Attribute 2</th>
+                        <th>Attribute 3</th>
+                        {/* Add more table headers as needed */}
+                    </tr>
+                </thead>
+                <tbody>
+                    {initialData.map((data, index) => (
+                        <tr className="table-row" key={index}>
+                            <td>
+                                <input type="text" value={data.attribute1} readOnly disabled={!enable} />
+                            </td>
+                            <td>
+                                <input type="text" value={data.attribute2} readOnly disabled={!enable}/>
+                            </td>
+                            <td>
+                                <input type="text" value={data.attribute3} readOnly disabled={!enable}/>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <div className="button-container">
+                <button onClick={checkDisable} disabled={!enable}>
+                    Disable
+                </button>
+                <button onClick={checkEnable} disabled={enable}>
+                    Enable
+                </button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
+
